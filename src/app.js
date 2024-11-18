76a4e5dd86ca7b7aba6ab4618e9972612cbd926a
bobcode4 <ksarathkumar1918@gmail.com>
2024-11-18 16:35:17 -0600

    get, post, patch, delete API's

--- src/app.js
+++ src/app.js
@@ -3,25 +3,57 @@ const connectDB = require("./config/database");
 const User = require("./models/user");
 const app = express();
 
-app.post('/signup', async (req,res) => {
-    const userObj = {
-        firstName : "Virat",
-        lastName : "Kohli",
-        emailId : "virat@tinder.com",
-        password : "Virat@123",
-    };
+app.use(express.json())
 
-    const user = new User(userObj);
+app.post('/signup', async (req,res) => {
+    const user = new User(req.body);
+
     try {
         await user.save();
         res.send("user saved");
     } catch (err){
         res.status(400).send("Error saving the user "+ err.message);
     }
+});
+
+app.get('/feed', async (req,res) => {
+    const users = await User.find({});
+    try {
+        if(users.length === 0){
+            res.status(404).send("Users not found")
+        } else {
+            res.send(users);
+        }     
+    }
+    catch {
+        res.send("Something went wrong")
+    }
+});
+
+app.delete('/user', async (req,res) => {
+    const userId = req.body.id;
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        console.log(user.isArray())
+        if(!user) res.send("user already deleted")
+        else res.send(type(user));
+    } catch (err) {
+        res.status(400).send("Something went wrong "+err.message)
+    }
 })
 
+app.patch('/user', async (req,res) => {
+    const {userId, emailId} = req.body;
+    const user = await User.findByIdAndUpdate(userId, {emailId : emailId}, {returnDocument : 'after'});
+    try{
+        if(!user) res.send("No user found with the given id")
+        else res.send(`updated user : ${user}\n user updated successfully`)
+    } catch(err) {
+        res.status(400).send("something went wrong " + err.message)
+    }
+}) 
 
-
+ 
 
 
 connectDB()
